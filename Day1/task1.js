ff5feccc4e92fca0c8423a7d12481c5058225351
AndreyKhramykh <khramykhandrey@gmail.com>
2025-12-03 01:19:14 +0200

    solutions of day 1

--- Day1/task1.js
+++ Day1/task1.js
@@ -1,7 +1,26 @@
 function getResult(arg) {
+	const data = arg.split('\n').map((elem) => {
+		return { direction: elem[0], steps: +elem.slice(1) }
+	})
+	let currentPosition = 50
+	let counter = 0
+
+	function getPosition(index) {
+		return ((index % 100) + 100) % 100
+	}
+
+	for (let i = 0; i < data.length; i++) {
+		data[i].direction == 'L'
+			? (currentPosition = getPosition(currentPosition - data[i].steps))
+			: (currentPosition = getPosition(currentPosition + data[i].steps))
+		currentPosition == 0 ? counter++ : null
+	}
+	// Get result
+	console.log(counter)
 }
 
 const fs = require('node:fs')
+const { clearLine } = require('node:readline')
 
 const samplePath = 'sample.txt'
 const fullPath = 'full.txt'
--- Day1/task2.js
+++ Day1/task2.js
@@ -1,4 +1,42 @@
 function getResult(arg) {
+	const data = arg.split('\n').map((elem) => {
+		return { direction: elem[0], steps: +elem.slice(1) }
+	})
+	let currentPosition = 50
+	let counter = 0
+
+	function getPosition(index) {
+		return ((index % 100) + 100) % 100
+	}
+	function countTransitions(
+		index,
+		direction,
+		currentPosition,
+		potentialPosition
+	) {
+		if (direction === 'R') {
+			return Math.floor(index / 100)
+		}
+
+		const coef =
+			(currentPosition === 0 ? -1 : 0) + (potentialPosition === 0 ? 1 : 0)
+
+		return Math.abs(Math.floor(index / 100)) + coef
+	}
+
+	for (let i = 0; i < data.length; i++) {
+		let index = 0
+		data[i].direction == 'L'
+			? (index = currentPosition - data[i].steps)
+			: (index = currentPosition + data[i].steps)
+
+		let potentialPosition = getPosition(index)
+		let params = [index, data[i].direction, currentPosition, potentialPosition]
+		counter += countTransitions(...params)
+		currentPosition = potentialPosition
+	}
+	// Get result
+	console.log('result', counter)
 }
 
 const fs = require('node:fs')
