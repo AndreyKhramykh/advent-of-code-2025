92655f6c745ac75d4e0577312e76e2e754f3f458
AndreyKhramykh <khramykhandrey@gmail.com>
2025-12-07 02:53:34 +0200

    solution of Day3 Task1. Task2 in proccess

--- Day3/task1.js
+++ Day3/task1.js
@@ -1,4 +1,37 @@
 function getResult(arg) {
+	function getData(data) {
+		return data.split('\n')
+	}
+	function getMaxDigit(string) {
+		let maxDigit = 0
+		let index = 0
+		for (let i = 0, length = string.length; i < length; i++) {
+			const digit = string.charCodeAt(i) - 48
+			if (digit > maxDigit) {
+				maxDigit = digit
+				index = i
+			}
+			if (maxDigit == 9) break
+		}
+		return { value: maxDigit, index: index }
+	}
+
+	function getJoltage(string) {
+		let firstDigit = getMaxDigit(string)
+		let secondDigit = {}
+		const length = string.length
+
+		if (firstDigit.index == length - 1) {
+			secondDigit = firstDigit
+			firstDigit = getMaxDigit(string.slice(0, firstDigit.index))
+		} else {
+			secondDigit = getMaxDigit(string.slice(firstDigit.index + 1))
+		}
+		return Number(String(firstDigit.value) + String(secondDigit.value))
+	}
+
+	let result = getData(arg).map((string) => getJoltage(string)).reduce((acc, curr) => acc + curr)
+	console.log(result)
 }
 
 const fs = require('node:fs')
@@ -6,7 +39,7 @@ const fs = require('node:fs')
 const samplePath = 'sample.txt'
 const fullPath = 'full.txt'
 
-fs.readFile(samplePath, 'utf-8', (error, data) => {
+fs.readFile(fullPath, 'utf-8', (error, data) => {
 	if (error) {
 		console.log(`output->error`, error)
 	}
--- Day3/task2.js
+++ Day3/task2.js
@@ -1,4 +1,47 @@
 function getResult(arg) {
+	function getData(data) {
+		return data.split('\n')
+	}
+	function getMaxDigit(string) {
+		let maxDigit = 0
+		let index = 0
+		for (let i = 0, length = string.length; i < length; i++) {
+			const digit = string.charCodeAt(i) - 48
+			if (digit > maxDigit) {
+				maxDigit = digit
+				index = i
+			}
+			if (maxDigit == 9) break
+		}
+		return { value: maxDigit, index: index }
+	}
+
+	function getJoltage(string) {
+		let currentDigit = getMaxDigit(string)
+		let nextDigit = {}
+		const length = string.length
+		let result = []
+
+		for (let k = 1; k < 13; k++) {
+			// console.log(currentDigit.value, currentDigit.index)
+			// console.log(nextDigit.value, nextDigit.index)
+			if (currentDigit.index == length - 1) {
+				nextDigit = currentDigit
+				currentDigit = getMaxDigit(string.slice(0, currentDigit.index))
+				// length--
+			} else {
+				nextDigit = getMaxDigit(string.slice(currentDigit.index + k))
+			}
+			result.push(currentDigit)
+			currentDigit = nextDigit
+		}
+
+		return result
+	}
+	console.log(getJoltage(getData(arg)[2]))
+
+	// let result = getData(arg).map((string) => getJoltage(string)).reduce((acc, curr) => acc + curr)
+	// console.log(result)
 }
 
 const fs = require('node:fs')
